b556e66640e586bff6eeec0d92cc5fbbb73a3cac
Yuta Kimura <52063678+kyuta8@users.noreply.github.com>
2022-07-08 18:13:38 +0900

    評価スクリプト実行のバッチファイル

---
+++ script/single.bat
@@ -0,0 +1,144 @@
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -p -c -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -m LR --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -m RF --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project influxdb -n 1
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -m LR --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -m RF --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project saleor -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -c -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -m LR --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -c -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -m RF --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -c -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project server -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -c -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -m LR --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -c -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -m RF --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -c -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -c -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -c -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project vscode -n 1
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -c -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m LR --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m RF --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project influxdb -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m LR --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m RF --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project saleor -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m LR --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m RF --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project server -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m LR --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m RF --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -p -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -c -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -c -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -r -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -p -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -c -m SVM --drop --project vscode -n 1 --tf
+python EvaluateSingleProjectModel.py -t -m SVM --drop --project vscode -n 1
